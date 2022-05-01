3c7bf506bbd3f7f2dcba8f873ddd3a058ac64405
Ajaykvishwakarma <ajaykv054@gmail.com>
2022-05-01 18:44:11 +0530

    I-F

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
     return mongoose.connect(
-        'mongodb://localhost:27017/flightOrfight'
+        'mongodb+srv://ajaykv:ajaykv@cluster0.t6wyj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     );
 }
 
